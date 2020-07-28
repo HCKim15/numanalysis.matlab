fbe1afab4eb88cec6a8078676e9ffd959bebd0a0
hyoung chul kim <andykent222@yonsei.ac.kr>
2020-07-28 19:59:45 +0900

    Create FDM_1D.m

---
+++ FDM_1D.m
@@ -0,0 +1,36 @@
+clear
+
+n=input('자연수를 입력하세요 : ');
+
+h=1/n;
+A=zeros(n-1);
+b=zeros(1,n-1);
+x=zeros(1,n+1);
+x(n+1)=1;
+
+for i=1:n-1
+    
+    x(i+1)=i*h;
+    b(i)=-(pi^2)*sin(pi*x(i+1));
+    
+    A(i,i:i+2)=[1,-2,1];
+        
+end
+
+A=A(:,2:n);
+
+b=b*(h^2);
+
+u=A\b';
+u=[0;u;0];
+
+figure(1)
+t=0:0.01:1; 
+plot(t,sin(pi*t),'b')
+xlabel('x')
+ylabel('u(x)')
+ylim([0,1.2])
+hold on
+plot(x,u,'.','MarkerSize',12)
+title(['Numerical solution to the Poisson equation with h=1/',num2str(1/h)])
+legend('exact', 'approx')
