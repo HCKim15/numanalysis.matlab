9569f74447ea31e78db6d904ea4fe488470eeb85
hyoung chul kim <andykent222@yonsei.ac.kr>
2020-07-28 21:26:14 +0900

    efficiency update(for loop)
    made two "for loop" into one by using slicing method on matrix A and deleting the unwanted columns. 
    
    Also updated the title section of the plot to show what value of n is used to get h.

--- fdm_1d.m
+++ fdm_1d.m
@@ -4,26 +4,19 @@ n=input('자연수를 입력하세요(input a natural number) : ');
 
 h=1/n;
 A=zeros(n-1,n-1);
-A(1,(1:2))=[-2, 1];
-A(n-1,(n-2:n-1))=[1,-2];
-b=zeros([1,n-1]);
-x=zeros([1,n+1]);
+b=zeros(1,n-1);
+x=zeros(1,n+1);
 x(n+1)=1;
 
 for i=1:n-1
     
     x(i+1)=i*h;
-    y=i*h;
-    b(i)=-(pi^2)*sin(pi*y);
-    
-    for j=2:n-2
-
-        A(j,(j-1:j+1))=[1,-2,1];
-        
-    end
+    b(i)=-(pi^2)*sin(pi*x(i+1));
+    A(i,i:i+2)=[1,-2,1];
     
 end
 
+A=A(:,2:n);
 b=b*(h^2);
 
 u=A\b';
@@ -37,5 +30,5 @@ ylabel('u(x)')
 ylim([0,1.2])
 hold on
 plot(x,u,'.','MarkerSize',12)
-title('Numerical solution to the Poisson equation')
+title(['Numerical solution to the Poisson equation with h=1/', num2str(1/h)])
 legend('exact', 'approx')
